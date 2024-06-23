138cc0c89adf187e050bcaaecf699b3c389f37cf
Kyle Scott <kyscott@umich.edu>
2024-06-23 15:33:45 -0400

    dumb compiler

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,7 +1,43 @@
 import type { Abi } from "abitype";
-import type { Program } from "./types/ast";
+import type { Ast } from "./types/ast";
 import type { Hex } from "./types/utils";
 
-export const compileAbi = (program: Program): Abi => {};
+export const compileAbi = (program: Ast.Program): Abi => {};
 
-export const compileCode = (program: Program): Hex => {};
+type Context = {
+  code: Hex;
+  /** Memory location for identifiers */
+  identifierLocation: Map<string, string>;
+};
+
+/**
+ * Compiles a valid Solidity program represented by an abstract
+ * syntax tree into EVM bytecode.
+ */
+export const compileCode = (program: Ast.Program): Hex => {
+  const context = {
+    code: "0x",
+    identifierLocation: new Map<string, string>(),
+  } as const;
+
+  for (const statement of program) {
+    if (statement.type === "expressionStatement") {
+      if (statement.expression.type === "identifier") {
+        compileIdentifier(context, statement.expression);
+      }
+    }
+  }
+
+  return context.code;
+};
+
+const compileIdentifier = (context: Context, identifier: Ast.Identifier) => {
+  // PUSH32 memory location [location]
+  const location = context.identifierLocation.get(identifier.token.lexeme)!;
+
+  // PUSH32 [location]: [location]
+  context.code += `7f${location}`;
+
+  // MLOAD            : [value]
+  context.code += "51";
+};
